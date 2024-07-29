5d1a5432b60f255f9297dd34f2044d370c5e97c9
KentHirose <k.hirose77777@gmail.com>
2024-07-30 06:30:29 +0900

    検索

--- main.py
+++ main.py
@@ -29,42 +29,62 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
 @app.get("/map")
-async def get_directions(origin="松戸駅", destination="渋谷駅", time="2024/8/1/11:00:00"):
+async def get_directions(
+    origin="松戸駅", destination="渋谷駅", time="2024/8/1/11:00:00"
+):
     return [
-            {
-                "origin": "松戸駅",
-                "destination": "日暮里駅",
-                "transportation": "常磐線",
-                "departure": "2024/8/1/10:00:00",
-                "arrival": "2024/8/1/10:30:00",
-            },
-            {
-              "origin": "日暮里駅常磐線ホーム",
-              "destination": "日暮里駅山手線ホーム",
-              "transportation": "乗り換え",
-              "departure": "2024/8/1/10:30:00",
-              "arrival": "2024/8/1/10:40:00",
-            },
-            {
-                "origin": "日暮里駅",
-                "destination": "渋谷駅",
-                "transportation": "山手線",
+        {
+            "origin": "松戸駅",
+            "destination": "日暮里駅",
+            "transportation": "常磐線",
+            "departure": "2024/8/1/10:00:00",
+            "arrival": "2024/8/1/10:30:00",
+        },
+        {
+            "origin": "日暮里駅常磐線ホーム",
+            "destination": "日暮里駅山手線ホーム",
+            "transportation": "乗り換え",
+            "departure": "2024/8/1/10:30:00",
+            "arrival": "2024/8/1/10:40:00",
+        },
+        {
+            "origin": "日暮里駅",
+            "destination": "渋谷駅",
+            "transportation": "山手線",
             "departure": "2024/8/1/10:40:00",
             "arrival": "2024/8/1/11:00:00",
-            }
+        },
     ]
-    
+
+
 @app.get("/route")
 async def get_route(origin: str, destination: str, name: str):
-    supabase.table("route").insert({
-      "origin": origin,
-      "destination": destination,
-      "name": name,
-    }).execute()
+    supabase.table("route").insert(
+        {
+            "origin": origin,
+            "destination": destination,
+            "name": name,
+        }
+    ).execute()
     return "ok"
+
+
+@app.get("/route_search")
+# nameの最新だけを取得する
+async def get_route_search(name: str):
+    res = (
+        supabase.table("route")
+        .select("*")
+        .eq("name", name)
+        .order("created_at", desc=True)
+        .limit(1)
+        .execute()
+    )
+    return res.data[0]
